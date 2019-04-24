897b125f9fc96ff0a84b9122637171907e05ad1c
sumariana <42775471+sumariana@users.noreply.github.com>
2019-04-24 14:20:53 +0700

    sparepart kelar

--- app/src/main/java/atmaauto/atmaauto/com/atmaauto/TambahKonsumen.java
+++ app/src/main/java/atmaauto/atmaauto/com/atmaauto/TambahKonsumen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class TambahKonsumen extends AppCompatActivity {
 
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
